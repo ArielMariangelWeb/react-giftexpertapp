836af7c70092d6c3f90d95fd495ffa3f671d0bc6
Ariel Mariangel <arielmariangelweb@gmail.com>
2020-09-06 18:15:20 -0300

    docs agregado

---
+++ docs/precache-manifest.7b3b047071945cf2ac630f89056db373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3de3356fbe587fa470cfc3506e22993",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a3d4f6082511c7ccafb",
+    "url": "/static/css/main.f66163eb.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3d4f6082511c7ccafb",
+    "url": "/static/js/main.78dff629.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
